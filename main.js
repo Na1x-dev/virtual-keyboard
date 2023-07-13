52ec87c97e53d3f24bc9e1c6358417020cd64039
Na1x <na1x.1024@gmail.com>
2023-07-13 16:17:29 +0300

    feat: set keys correct width and add animations

--- main.js
+++ main.js
@@ -15,19 +15,28 @@ const keyCodes = [
 
 const russianLayout = [
     ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-    ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'Del'],
+    ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\'],
     ['CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'],
-    ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '↑', 'Shift'],
+    ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', 'Shift','↑', 'Del'],
     ['Ctrl', 'Meta', 'Alt', ' ', 'Alt', 'Ctrl', '←', '↓', '→']
 ];
 
 const englishLayout = [
     ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-    ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del'],
+    ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\'],
     ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'Enter'],
-    ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '↑', 'Shift'],
+    ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'Shift', '↑', 'Del'],
     ['Ctrl', 'Meta', 'Alt', ' ', 'Alt', 'Ctrl', '←', '↓', '→']
 ];
+
+const multiplierWidth = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2.3],
+    [1.5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.8],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2.5],
+    [2.3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1.6, 1, 1, 7.1, 1, 1.6, 1, 1, 1]
+];
+
 
 let keys =
     [['`', '1', '2', '3'],
@@ -55,8 +64,8 @@ mainBlock.appendChild(inputBlock);
 //     console.log(key);
 // }
 
-function createInputLines(){
-    for(let i=0; i<5; i++){
+function createInputLines() {
+    for (let i = 0; i < 5; i++) {
         var line = document.createElement('div');
         line.className = 'input-line';
         inputBlock.appendChild(line);
@@ -65,23 +74,26 @@ function createInputLines(){
 }
 
 function createVirtualKeys(layout) {
+
     for (let i = 0; i < layout.length; i++) {
+        let j = 0;
         for (let key of layout[i]) {
             var keyDiv = document.createElement('div');
             keyDiv.className = 'key-div';
             keyDiv.appendChild(document.createTextNode(key));
             inputLines[i].appendChild(keyDiv);
-
-            keyDiv.style.width = keyDiv.offsetHeight + "px";
-            inputLines[i].appendChild(keyDiv);
-            // console.log(keyDiv.offsetW);            
+            keyDiv.style.width = keyDiv.offsetHeight * multiplierWidth[i][j] + "px";
+            j++;
         }
     }
 }
 
-function main(){
+
+
+function main() {
     createInputLines();
     createVirtualKeys(englishLayout);
+    outputBlock.style.width = inputBlock.offsetWidth-26 + "px";
 }
 
 main();
