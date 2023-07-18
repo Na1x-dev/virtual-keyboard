594dc60f9f90eac7720775792d4824dbaa10d993
Na1x <na1x.1024@gmail.com>
2023-07-18 15:46:18 +0300

    feat: add shift layout

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const keyCodes = [
     ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
     ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash'],
     ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
-    ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight','ArrowUp', 'Delete'],
+    ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight', 'ArrowUp', 'Delete'],
     ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight']
 ];
 
@@ -21,11 +21,27 @@ const russianLayout = [
     ['Ctrl', 'Meta', 'Alt', ' ', 'Alt', 'Ctrl', '←', '↓', '→']
 ];
 
+const russianShiftLayout = [
+    ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+    ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\'],
+    ['CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'],
+    ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', 'Shift', '↑', 'Del'],
+    ['Ctrl', 'Meta', 'Alt', ' ', 'Alt', 'Ctrl', '←', '↓', '→']
+];
+
 const englishLayout = [
     ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-    ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\'],
-    ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'Enter'],
-    ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'Shift', '↑', 'Del'],
+    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
+    ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
+    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift', '↑', 'Del'],
+    ['Ctrl', 'Meta', 'Alt', ' ', 'Alt', 'Ctrl', '←', '↓', '→']
+];
+
+const englisShiftLayout = [
+    ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'],
+    ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|'],
+    ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'],
+    ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Shift', '↑', 'Del'],
     ['Ctrl', 'Meta', 'Alt', ' ', 'Alt', 'Ctrl', '←', '↓', '→']
 ];
 
@@ -37,20 +53,14 @@ const multiplierWidth = [
     [1.6, 1, 1, 'auto', 1, 1.6, 1, 1, 1]
 ];
 
-
-// let keys =
-//     [['`', '1', '2', '3'],
-//     [],
-//     [],
-//     [],
-//     []];
+var layouts = [[englishLayout, englisShiftLayout], [russianLayout, russianShiftLayout]];
 
 mainBlock.className = 'main-block';
 header.className = 'main-header';
 outputBlock.className = 'output-block';
 outputField.className = 'output-field';
 inputBlock.className = 'input-block';
-outputField.autofocus = 'autofocus';
+outputField.readOnly = 'readonly';
 
 header.innerHTML = "Virtual Keyboard";
 
@@ -80,7 +90,7 @@ function createVirtualKeys(layout) {
         for (let key of layout[i]) {
             var keyDiv = document.createElement('div');
             keyDiv.className = 'key-div';
-            keyDiv.appendChild(document.createTextNode(key));
+            keyDiv.innerHTML = key;
             inputLines[i].appendChild(keyDiv);
             if (multiplierWidth[i][j] !== 'auto') {
                 keyDiv.style.minWidth = keyDiv.offsetHeight * multiplierWidth[i][j] + "px";
@@ -92,8 +102,24 @@ function createVirtualKeys(layout) {
     }
 }
 
+// function writeToTheOutputField(i, j){
+//     if
+//     outputField.value += englishLayout[i][j];
+// }
+
 function getKeyDiv(i, j) {
     return inputLines[i].getElementsByClassName('key-div')[j]
+}
+
+function changeLayout(language, shift){
+var layout = layouts[language][shift];
+    for (let i = 0; i < layout.length; i++) {
+        let j = 0;
+        for (let key of layout[i]) {
+            getKeyDiv(i, j).innerHTML = key;
+            j++;
+        }
+    }
 }
 
 document.addEventListener('keydown', function (event) {
@@ -102,17 +128,25 @@ document.addEventListener('keydown', function (event) {
             if (event.code == keyCodes[i][j]) {
                 getKeyDiv(i, j).style.backgroundColor = "#eee";
                 getKeyDiv(i, j).style.color = "#333";
+                if(event.shiftKey){
+                    changeLayout(0, 1);
+                }
             }
+            
     }
 });
 
 document.addEventListener('keyup', function (event) {
     for (let i = 0; i < keyCodes.length; i++) {
         for (let j = 0; j < keyCodes[i].length; j++)
-            if (event.code == keyCodes[i][j]) {
+            if (event.code === keyCodes[i][j]) {
                 getKeyDiv(i, j).style.backgroundColor = "#557";
                 getKeyDiv(i, j).style.color = "#eee";
+                if(!event.shiftKey){
+                    changeLayout(0, 0);
+                }
             }
+            
     }
 });
 
@@ -120,6 +154,7 @@ function main() {
     createInputLines();
     createVirtualKeys(englishLayout);
     outputBlock.style.width = inputBlock.offsetWidth - 26 + "px";
+
 }
 
 main();
