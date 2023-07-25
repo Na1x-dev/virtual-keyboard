a38442e71b82b106959e1c5ddb6b88aaf82a273d
Na1x <na1x.1024@gmail.com>
2023-07-25 19:52:41 +0300

    feat: add themes

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ var header = document.createElement('h1');
 var outputBlock = document.createElement('div');
 var inputBlock = document.createElement('div');
 var outputField = document.createElement('textarea');
+var themeButtonContainer = document.createElement('div');
 var inputLines = [];
+var shiftPressed = 0;
+//background, field, text, hover, active
+var themeColors = [['#557', '#668', '#eee', '#446', '#333'], ['#575', '#686', '#eee', '#464', '#333'], ['#755', '#866', '#eee', '#644', '#333'], ['#eee', '#eee', '#333', '#999', '#eee'], ['#333', '#333', '#eee', '#555', '#333']];
+var chosedTheme = 0;
 
 const keyCodes = [
     ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
@@ -55,6 +60,7 @@ const multiplierWidth = [
 
 var layouts = [[englishLayout, englisShiftLayout], [russianLayout, russianShiftLayout]];
 
+themeButtonContainer.className = 'theme-button-container';
 mainBlock.className = 'main-block';
 header.className = 'main-header';
 outputBlock.className = 'output-block';
@@ -64,16 +70,13 @@ outputField.readOnly = 'readonly';
 
 header.innerHTML = "Virtual Keyboard";
 
+document.body.appendChild(themeButtonContainer);
 document.body.append(mainBlock);
 mainBlock.appendChild(header);
 mainBlock.appendChild(outputBlock);
 outputBlock.appendChild(outputField);
 mainBlock.appendChild(inputBlock);
 
-// function handleKeyDown(event) {
-//     var key = event.key;
-//     console.log(key);
-// }
 
 function createInputLines() {
     for (let i = 0; i < 5; i++) {
@@ -81,6 +84,40 @@ function createInputLines() {
         line.className = 'input-line';
         inputBlock.appendChild(line);
         inputLines.push(line);
+    }
+}
+
+function createThemeButtons() {
+    for (let i = 0; i < themeColors.length; i++) {
+        var themeButton = document.createElement('div');
+        themeButton.className = 'theme-button';
+        themeButton.onclick = function () {
+            chosedTheme = i;
+            changeTheme();
+        };
+        themeButton.style.backgroundColor = themeColors[i][0];
+        themeButton.style.borderColor = themeColors[i][2];
+        themeButtonContainer.appendChild(themeButton);
+    }
+}
+
+function changeTheme() {
+    document.body.style.backgroundColor = themeColors[chosedTheme][0];
+    outputField.style.backgroundColor = themeColors[chosedTheme][1];
+    outputField.style.color = themeColors[chosedTheme][2];
+    inputBlock.style.backgroundColor =  themeColors[chosedTheme][1];
+    outputField.style.borderColor = themeColors[chosedTheme][3];
+    inputBlock.style.borderColor =  themeColors[chosedTheme][3];
+    themeButtonContainer.style.backgroundColor =  themeColors[chosedTheme][1];
+    themeButtonContainer.style.borderColor =  themeColors[chosedTheme][3];
+    for (let i = 0; i < keyCodes.length; i++) {
+        for (let j = 0; j < keyCodes[i].length; j++) {
+            getKeyDiv(i, j).style.backgroundColor = themeColors[chosedTheme][0];
+            getKeyDiv(i, j).style.borderColor = themeColors[chosedTheme][3];
+            getKeyDiv(i, j).style.color = themeColors[chosedTheme][2];
+            
+            
+        }
     }
 }
 
@@ -102,10 +139,15 @@ function createVirtualKeys(layout) {
     }
 }
 
-// function writeToTheOutputField(i, j){
-//     if
-//     outputField.value += englishLayout[i][j];
-// }
+function writeToTheOutputField(i, j) {
+
+    if (keyCodes[i][j].includes('Digit') || keyCodes[i][j].includes('Key') || keyCodes[i][j] === 'Backquote' || keyCodes[i][j] === 'Minus' || keyCodes[i][j] === 'Equal' || keyCodes[i][j] === 'BracketLeft' || keyCodes[i][j] === 'BracketRight' || keyCodes[i][j] === 'Backslash' || keyCodes[i][j] === 'Semicolon' || keyCodes[i][j] === 'Quote' || keyCodes[i][j] === 'Comma' || keyCodes[i][j] === 'Period' || keyCodes[i][j] === 'Slash' || keyCodes[i][j] === 'Space') {
+        outputField.value += layouts[0][shiftPressed][i][j];
+    }
+    else if (keyCodes[i][j] === 'Backspace') {
+        outputField.value = outputField.value.slice(0, -1);
+    }
+}
 
 function getKeyDiv(i, j) {
     return inputLines[i].getElementsByClassName('key-div')[j]
@@ -123,37 +165,59 @@ function changeLayout(language, shift) {
 }
 
 function setPressedButtonColor(i, j) {
-    getKeyDiv(i, j).style.backgroundColor = "#eee";
-    getKeyDiv(i, j).style.color = "#333";
+    getKeyDiv(i, j).style.backgroundColor = themeColors[chosedTheme][2];
+    getKeyDiv(i, j).style.color = themeColors[chosedTheme][4];
+    if (keyCodes[i][j] === 'Shift') {
+        shiftPressed = 1;
+    }
 }
 
 function setUnpressedButtonColor(i, j) {
-    getKeyDiv(i, j).style.backgroundColor = "#557";
-    getKeyDiv(i, j).style.color = "#eee";
+    getKeyDiv(i, j).style.backgroundColor = themeColors[chosedTheme][0];
+    getKeyDiv(i, j).style.color = themeColors[chosedTheme][2];
 }
 
 function setHoverButtonColor(i, j) {
-    getKeyDiv(i, j).style.backgroundColor = '#446';
-    getKeyDiv(i, j).style.color = "#eee";
+    getKeyDiv(i, j).style.backgroundColor = themeColors[chosedTheme][3];
+    getKeyDiv(i, j).style.color = themeColors[chosedTheme][2];
+}
+
+function setUnPressedButtonColorWithMouse(i, j) {
+    getKeyDiv(i, j).style.backgroundColor = themeColors[chosedTheme][3];
+    getKeyDiv(i, j).style.color = themeColors[chosedTheme][2];
+}
+
+function setUnHoverButtonColor(i, j) {
+    getKeyDiv(i, j).style.backgroundColor = themeColors[chosedTheme][0];
+    getKeyDiv(i, j).style.color = themeColors[chosedTheme][2];
+}
+
+function checkShiftForMouse(i, j, shiftPressedDefault) {
+    if (keyCodes[i][j] === 'ShiftLeft' || keyCodes[i][j] === 'ShiftRight') {
+        shiftPressed = shiftPressedDefault;
+    }
 }
 
 
 function setEventListenerToVirtualKey() {
     for (let i = 0; i < keyCodes.length; i++) {
         for (let j = 0; j < keyCodes[i].length; j++) {
-
             getKeyDiv(i, j).addEventListener("mousedown", function () {
+                checkShiftForMouse(i, j, 1);
+                changeLayout(0, shiftPressed);
+                writeToTheOutputField(i, j);
                 setPressedButtonColor(i, j);
-                
             });
             getKeyDiv(i, j).addEventListener("mouseup", function () {
-                setUnpressedButtonColor(i, j);
+                checkShiftForMouse(i, j, 0);
+                setUnPressedButtonColorWithMouse(i, j);
+                changeLayout(0, shiftPressed);
             });
             getKeyDiv(i, j).addEventListener("mouseover", function () {
                 setHoverButtonColor(i, j);
             });
             getKeyDiv(i, j).addEventListener("mouseout", function () {
-                setUnpressedButtonColor(i, j);
+                setUnHoverButtonColor(i, j);
             });
         }
     }
@@ -163,10 +227,12 @@ document.addEventListener('keydown', function (event) {
     for (let i = 0; i < keyCodes.length; i++) {
         for (let j = 0; j < keyCodes[i].length; j++)
             if (event.code == keyCodes[i][j]) {
-                setPressedButtonColor(i, j);
                 if (event.shiftKey) {
-                    changeLayout(0, 1);
+                    shiftPressed = 1;
                 }
+                writeToTheOutputField(i, j);
+                setPressedButtonColor(i, j);
+                changeLayout(0, shiftPressed);
             }
     }
 });
@@ -175,19 +241,21 @@ document.addEventListener('keyup', function (event) {
     for (let i = 0; i < keyCodes.length; i++) {
         for (let j = 0; j < keyCodes[i].length; j++)
             if (event.code === keyCodes[i][j]) {
-                setUnpressedButtonColor(i, j);
                 if (!event.shiftKey) {
-                    changeLayout(0, 0);
+                    shiftPressed = 0;
                 }
+                setUnpressedButtonColor(i, j);
+                changeLayout(0, shiftPressed);
+
             }
     }
 });
 
 function main() {
-
+    createThemeButtons();
     createInputLines();
     createVirtualKeys(englishLayout);
-
+changeTheme();
     outputBlock.style.width = inputBlock.offsetWidth - 26 + "px";
     setEventListenerToVirtualKey();
 }
